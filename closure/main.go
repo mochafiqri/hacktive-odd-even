06ab95debf7bf640857d66873372e1d731a384de
mochafiqri <mocha.fiqri@gmail.com>
2022-08-25 20:34:56 +0700

    assignment 1

--- closure/main.go
+++ closure/main.go
@@ -1,23 +1,62 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+)
 
 type Member struct {
-	Name string
+	Name    string
+	Address string
+	Job     string
+	Reason  string
 }
 
 func main() {
-	var team = []string{"Rijal", "Clara", "Adit", "Ivan", "Yudha", "Eka", "Fathur", "Sigit", "Giva", "Inas"}
 
-	var members []*Member
-	var addMember = func(name string) {
-		members = append(members, &Member{Name: name})
-	}
-	for _, v := range team {
-		addMember(v)
+	index := os.Args[1:]
+	var i = 0
+	var err error
+	if len(index) > 0 {
+		i, err = strconv.Atoi(index[0])
+		if err != nil {
+			log.Fatalln(err)
+		}
 	}
 
-	for _, v := range members {
-		fmt.Println("Hai : ", v.Name)
+	var team = []*Member{{
+		Name:    "Fiqri",
+		Address: "Bandung",
+		Job:     "Backend",
+		Reason:  "Keren",
+	},
+		{
+			Name:    "Clara",
+			Address: "Bekasi",
+			Job:     "Frontend",
+			Reason:  "Keren",
+		},
+		{
+			Name:    "Rijal",
+			Address: "Pinrang",
+			Job:     "Haha Hihi",
+			Reason:  "Keren",
+		}}
+
+	var print = func(i int) {
+		i--
+		if len(team) < i || i < 0 {
+			fmt.Println("Team ga ada")
+			return
+		}
+		fmt.Println("Name : ", team[i].Name)
+		fmt.Println("Address : ", team[i].Name)
+		fmt.Println("Job : ", team[i].Name)
+		fmt.Println("Reason : ", team[i].Name)
 	}
+
+	print(i)
+
 }
